87bc43f2cb072653990d2769901b45d66282fa60
Nils Gandlau <nilsgandlau@gmail.com>
2018-11-26 18:22:36 +0100

    Update

--- viergewinnt.py
+++ viergewinnt.py
@@ -1,50 +1,60 @@
 import numpy as np
+from string import ascii_letters
 
 class Player:
-	"""Creates Player Instances for the game 4-Gewinnt"""
+	"""Creates Player Instances for the game 4-Gewinnt."""
 
 	def __init__(self):
 		self.name = "Default"
-		self.piece_type = None  # Player has either 1's or 2's as pieces he throws into the board. 0's indicate empty
-    
+		self.piece_type = None
+
 	def set_playername(self):
-		"""Let's the user choose his own playernickname"""
-    	pass
-    
+		"""Let's the player pick his own Nickname"""
+		name = input("What name do you want to give yourself? " + 
+					 "(between 4 and 20 characters; only ASCII-Letters): ")
+		
+		isAscii = [True for letter in name if letter in ascii_letters]
+		while (len(name)>20 or len(isAscii) != len(name)):
+			print("Sorry. Try again.")
+			name = input("What name do you want to give yourself? " + 
+					 	"(between 4 and 20 characters; only ASCII-Letters): ")
+			isAscii = [True for letter in name if letter in ascii_letters]
+		
+		self.name = name
+
 	def move(self, col):
 		"""Let's the player choose the column in which he wants to put his
-		piece. Then checks whether this is a valid move. If yes, it updates the
-		board.
+		piece. Then checks whether this move is valid. If yes, it updates
+		the board.
 		"""
-    	pass
-  
+
 class Board:
-	"""Represents a board in the game 4-Gewinnt"""
-
-  	def __init__(self):
-		self.board = np.zeros((9, 10))
-		
-
-	def check_for_winning_patterns(self):
-		"""Checks whether one of the players one of the four winning patterns.
-		"""
-		pass
-	
-class Game:
-	"""The actual game of 4-Gewinnt"""
+	"""Represents a (9 X 10) board in the game"""
 
 	def __init__(self):
-		self.board = Board()	# Creates a Board for the game
-		self.players = []  		# List that contains players
-		self.initialize_game()	
-		
-	def initialize_game(self):
-		"""Initialization process of the game consists of x steps:
-		1. Ask whether user wants to start a Multiplayer or Singleplayer Game.
-		2. Ask whether user(s) want DEFAULT playernames or want to set custom
-		   nicknames.
-		3. Either way, we have to ask the first player what type of piece
-		   he prefers (player.piece_type). He can choose between 1 and 2. The
-		   other player then get's the piece_type that is left.
-		4. Starts the game.
+		self.board = np.zeros((9, 10))
+
+	def check_for_winning_patterns(self):
+		"""Checks whether one of the players has one of the four winning
+		patterns.
 		"""
+
+class Game:
+	"""The actual game of 4-Gewinnt."""
+
+	def __init__(self):
+		self.board = Board()
+		self.players = []
+		self.initialize_game()
+
+	def initialize_game(self):
+		"""Initialization process consists of the following steps:
+
+		1. Ask if user wants to play Multiplayer or Singleplayer game.float
+		2. Ask user if he wants to customize Nicknames. Otherwise generate
+		   default names.
+		3. Ask user what piece type he wants (player.piece_type). This can be
+		   either 1's or 2's (zeros on the board represent empty fields).
+		4. Start the game.
+		"""
+		
